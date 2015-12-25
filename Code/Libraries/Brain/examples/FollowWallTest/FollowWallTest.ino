86e31df4219946fd24bdddb3667b11684ba0484d
BryceHennen <ecyrbnenneh@gmail.com>
2015-12-25 15:34:18 -0600

    Create empty test sketch

---
+++ Code/Libraries/Brain/examples/FollowWallTest/FollowWallTest.ino
@@ -0,0 +1,28 @@
+#include <Brain.h>
+#include <Sensors.h>
+#include <Motors.h>
+#include <MotorDriver.h>
+#include <Arduino.h>
+#include <Pixy.h>
+#include <EEPROM.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <L3G.h>
+
+///////////////////////////////////////////////////////////
+// Tests the FollowWall() function of the Brain library. //
+///////////////////////////////////////////////////////////
+
+
+
+// the setup function runs once when you press reset or power the board
+void setup() 
+{
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() 
+{
+  
+}
