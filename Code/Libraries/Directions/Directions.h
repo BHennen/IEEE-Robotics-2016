f890603311c8e1ac507eaddde28fffe13691d955
BryceHennen <ecyrbnenneh@gmail.com>
2016-01-25 13:12:55 -0600

    Added code to transform a diretion given a rotation (left or right). This allows us to give the robot directions easier.

--- Code/Libraries/Directions/Directions.h
+++ Code/Libraries/Directions/Directions.h
@@ -6,10 +6,30 @@
 //Directions to turn, wall follow, etc
 enum Direction : byte
 {
-	UP,
-	RIGHT,
-	DOWN,
-	LEFT
+	UP = 0,
+	RIGHT = 1,
+	DOWN = 2,
+	LEFT = 3
 };
 
+//Given an old robot direction, finds what the new direction would be after a rotation
+Direction MapRotationToNewDirection(Direction rotation, Direction old_direction);
+inline Direction MapRotationToNewDirection(Direction rotation, Direction old_direction)
+{
+	if(rotation == RIGHT)
+	{
+		return static_cast<Direction>((old_direction + 1) % 4);
+	}
+	else
+	{
+		if(old_direction == UP)
+		{
+			return LEFT;
+		}
+		else
+		{
+			return static_cast<Direction>((old_direction - 1));
+		}
+	}
+}
 #endif
