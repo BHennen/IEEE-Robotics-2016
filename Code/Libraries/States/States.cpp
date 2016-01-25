55ba12e04fdcb1839252602ed22c40b35296a8b4
BryceHennen <ecyrbnenneh@gmail.com>
2016-01-25 13:05:16 -0600

    Added board state class and a RobotStateSet class. Also added extra bit of information to robot state (if it is on top of a victim).

--- Code/Libraries/States/States.cpp
+++ Code/Libraries/States/States.cpp
@@ -2,11 +2,12 @@
 
 //Constructors /////////////////////
 //Constructor with initial state given a direction and x and y position
-RobotState::RobotState(Direction dir, byte x, byte y)
+RobotState::RobotState(Direction dir, byte x, byte y, bool on_victim/* = false*/)
 {
 	SetDirection(dir);
 	SetX(x);
 	SetY(y);
+	SetOnVictim(on_victim);
 }
 
 //Default (assume we're at start location)
@@ -31,8 +32,8 @@ RobotState& RobotState::operator= (const RobotState& rhs)
 //Setters //////////////////
 
 //Set leftmost bits of bitset representing our direction
-//Direction:	76543210	00 = Up		01 = Right
-//				XX------	10 = Down	10 = Left
+//Direction:	876543210	00 = Up		01 = Right
+//				-XX------	10 = Down	10 = Left
 void RobotState::SetDirection(Direction dir)
 {
 	switch(dir)
@@ -57,8 +58,8 @@ void RobotState::SetDirection(Direction dir)
 }
 
 //Set middle 3 bits of bitset representing our x position
-//X:	76543210
-//		--XXX---
+//X:	876543210
+//		---XXX---
 void RobotState::SetX(byte x)
 {
 	//Check if third bit of x is set
@@ -93,8 +94,8 @@ void RobotState::SetX(byte x)
 }
 
 //Set last 3 bits of bitset representing our y position
-//Y:	76543210
-//		-----XXX
+//Y:	876543210
+//		------XXX
 void RobotState::SetY(byte y)
 {
 	//Check if third bit of y is set
@@ -128,12 +129,20 @@ void RobotState::SetY(byte y)
 	}
 }
 
+//Set whenever we're on top of a victim
+//on:	876543210
+//		X--------
+void RobotState::SetOnVictim(bool on_victim) 
+{
+	bits_[8] = on_victim;
+}
+
 //Getters //////////////////////
 
 //Get leftmost bits of bitset representing our direction
-//Direction:	76543210	00 = Up		01 = Right
-//				XX------	10 = Down	11 = Left
-Direction RobotState::GetDirection()
+//Direction:	876543210	00 = Up		01 = Right
+//				-XX------	10 = Down	11 = Left
+Direction RobotState::GetDirection() const
 {
 	if(bits_.test(7))
 	{
@@ -162,19 +171,192 @@ Direction RobotState::GetDirection()
 }
 
 //Get middle 3 bits of bitset representing our x position
-//X:	76543210
-//		--XXX---
-byte RobotState::GetX()
+//X:	876543210
+//		---XXX---
+byte RobotState::GetX() const
 {
 	//(1 or 0) * 4 + (1 or 0) * 2 + (1 or 0) * 1
 	return bits_.test(5) * static_cast<byte>(4) + bits_.test(4) * static_cast<byte>(2) + bits_.test(3) * static_cast<byte>(1);
 }
 
 //Set last 3 bits of bitset representing our y position
-//Y:	76543210
-//		-----XXX
-byte RobotState::GetY()
+//Y:	876543210
+//		------XXX
+byte RobotState::GetY() const
 {
 	//(1 or 0) * 4 + (1 or 0) * 2 + (1 or 0) * 1
 	return bits_.test(2) * static_cast<byte>(4) + bits_.test(1) * static_cast<byte>(2) + bits_.test(0) * static_cast<byte>(1);
+}
+
+//Return true when we're on top of a victim.
+//on:	876543210
+//		X--------
+bool RobotState::IsOnVictim() const
+{
+	return bits_[8];
+}
+
+//BoardState//////////////////////
+
+//Constructor
+BoardState::BoardState()
+{
+	//Create 8x8 grid of empty bitsets
+	for(int row = 0; row < 8; row++)
+	{
+		for(int col = 0; col < 8; col++)
+		{
+			arena_[row][col] = std::bitset<BOARD_STATE_SIZE>();
+		}
+	}
+}
+
+//Constructor for initial states
+BoardState::BoardState(std::bitset<BOARD_STATE_SIZE> init_states[64])
+{
+	//fill in arena left to right, top to bottom from the list
+	//It should be like a standard coordinate system, with start at [0][0]
+	for(int row = 0; row < 8; row++)
+	{
+		for(int col = 0; col < 8; col++)
+		{
+			arena_[7 - row][col] = init_states[row * 8 + col];
+		}
+	}
+};
+
+//Test if a grid unit has a wall in specified direction
+//		76543210
+//		URDL----
+bool BoardState::HasWall(byte x, byte y, Direction dir) const
+{
+	byte val;
+	if(dir == UP)
+	{
+		val = 7;
+	}
+	else if(dir = RIGHT)
+	{
+		val = 6;
+	}
+	else if(dir = DOWN)
+	{
+		val = 5;
+	}
+	else if(dir = LEFT)
+	{
+		val = 4;
+	}
+	return arena_[x][y][val];
+}
+
+//Check if grid unit is yellow location drop zone
+//		76543210
+//		----Y---
+bool BoardState::IsYellow(byte x, byte y) const
+{
+	return arena_[x][y][3];
+}
+
+//Check if grid unit is red location drop zone
+//		76543210
+//		-----R--
+bool BoardState::IsRed(byte x, byte y) const
+{
+	return arena_[x][y][2];
+}
+
+//Check if grid unit is a victim location
+//		76543210
+//		------V-
+bool BoardState::HasVictim(byte x, byte y) const
+{
+	return arena_[x][y][1];
+}
+
+//Check if grid unit is a passable location (meaning the robot can move through it)
+//		76543210
+//		-------P
+bool BoardState::IsPassable(byte x, byte y) const
+{
+	return arena_[x][y][0];
+}
+
+//Set the state of where the right victim is.
+void BoardState::SetRightVictimLocation(bool victim_is_up)
+{
+	if(victim_is_up)
+	{
+		//Victim in the right upper position is there and no longer passable.
+		arena_[5][7][1] = 1;
+		arena_[5][7][0] = 0;
+		//Remove lower victim and make passable
+		arena_[7][5][1] = 0;
+		arena_[7][5][0] = 1;
+	}
+	else
+	{
+		//Victim in the right lower position is there and no longer passable.
+		arena_[7][5][1] = 1;
+		arena_[7][5][0] = 0;
+		//Remove upper victim and make passable
+		arena_[5][7][1] = 0;
+		arena_[5][7][0] = 1;
+	}
+}
+
+//Set the state of where the left victim is.
+void BoardState::SetLeftVictimLocation(bool victim_is_up)
+{
+	if(victim_is_up)
+	{
+		//Victim in the left upper position is there and no longer passable.
+		arena_[0][5][1] = 1;
+		arena_[0][5][0] = 0;
+		//Remove lower victim and make passable
+		arena_[0][3][1] = 0;
+		arena_[0][3][0] = 1;
+	}
+	else
+	{
+		//Victim in the left lower position is there and no longer passable.
+		arena_[0][3][1] = 1;
+		arena_[0][3][0] = 0;
+		//Remove upper victim and make passable
+		arena_[0][5][1] = 0;
+		arena_[0][5][0] = 1;
+	}
+}
+
+//Set flag for grid position at [x][y] for the victim to false and make passable.
+void BoardState::RemoveVictim(byte x, byte y)
+{
+	arena_[x][y][1] = 0;
+	arena_[x][y][0] = 1;
+}
+
+RobotStateSet::RobotStateSet()
+{
+
+}
+
+//Add copy of state to the set (if possible, returns true, otherwise returns false)
+//Return true if successfully added
+//Return false if set already contains this state
+bool RobotStateSet::Add(const RobotState& state)
+{
+	if(this->Contains(state)) return false;
+
+	set_.push_back(state);
+	return true;
+}
+
+//Return true if the set already has this state.
+bool RobotStateSet::Contains(const RobotState& state)
+{
+	for(std::vector<RobotState>::const_iterator it = set_.begin(); it != set_.end(); ++it)
+	{
+		if(*it == state) return true;
+	}
+	return false;
 }
--- Code/Libraries/States/States.h
+++ Code/Libraries/States/States.h
@@ -3,14 +3,19 @@
 
 #include <iterator>
 #include <bitset>
+#include <vector>
 #include <Directions.h>
 
+static const size_t ROBOT_STATE_SIZE = 9;
+static const size_t BOARD_STATE_SIZE = 8;
+
+//Class representing the state of a robot (direction and position)
 class RobotState
 {
 public:
 	//Constructors ////////////////////
 	//Constructor given all paramaters
-	RobotState(Direction dir, byte x, byte y);
+	RobotState(Direction dir, byte x, byte y, bool on_victim = false);
 
 	//Default (assume we're at start location)
 	RobotState();
@@ -21,6 +26,11 @@ public:
 	//Overload assignment operator
 	RobotState& operator= (const RobotState& rhs);
 
+	inline bool operator== (const RobotState& rhs) const
+	{
+		return bits_ == rhs.bits_;
+	};
+
 	//Setters //////////////////
 	void SetDirection(Direction dir);
 
@@ -28,16 +38,77 @@ public:
 
 	void SetY(byte y);
 
+	//Set whenever we're on top of a victim
+	void SetOnVictim(bool on_victim);
+
 	//Getters //////////////////////
-	Direction GetDirection();
+	Direction GetDirection() const;
 
-	byte GetX();
+	byte GetX() const;
 
-	byte GetY();
+	byte GetY() const;
+
+	//Return true when we're on top of a victim.
+	bool IsOnVictim() const;
 
 private:
-	static const size_t STATE_SIZE = 8;
-	std::bitset<STATE_SIZE> bits_;
+	std::bitset<ROBOT_STATE_SIZE> bits_;
 };
 
+//Class that represents the sate of the board.
+class BoardState
+{
+public:
+	BoardState();
+
+	BoardState(std::bitset<BOARD_STATE_SIZE> init_states[64]);
+
+	inline bool operator== (const BoardState& rhs) const
+	{
+		return arena_ == rhs.arena_;
+	};
+
+	//Test if a grid unit has a wall in specified direction
+	bool HasWall(byte x, byte y, Direction dir) const;
+
+	//Check if grid unit is yellow location drop zone
+	bool IsYellow(byte x, byte y) const;
+
+	//Check if grid unit is red location drop zone
+	bool IsRed(byte x, byte y) const;
+
+	//Check if grid unit is a victim location
+	bool HasVictim(byte x, byte y) const;
+
+	//Check if grid unit is a passable location (meaning the robot can move through it)
+	bool IsPassable(byte x, byte y) const;
+
+	//Set the state of where the right victim is.
+	void SetRightVictimLocation(bool victim_is_up);
+
+	//Set the state of where the left victim is.
+	void SetLeftVictimLocation(bool victim_is_up);
+
+	//Set flag for grid position at [x][y] for the victim to false
+	void RemoveVictim(byte x, byte y);
+
+private:
+	std::bitset<BOARD_STATE_SIZE> arena_[8][8];
+};
+
+class RobotStateSet
+{
+public:
+	RobotStateSet();
+
+	//Add copy of state to the set (if possible, returns true, otherwise returns false)
+	//Return true if successfully added
+	//Return false if set already contains this state
+	bool Add(const RobotState& state);
+
+	//Return true if the set already has this state.
+	bool Contains(const RobotState& state);
+private:
+	std::vector<RobotState> set_;
+};
 #endif
